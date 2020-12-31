eb2913acee99a378b633319ffb050a29bd307bff
VladimirKing <cheremuha1@gmail.com>
2020-12-31 22:00:30 +0300

    Learning

--- src/Learning.java
+++ src/Learning.java
@@ -13,36 +13,28 @@ static ArrayList<GetSet>learEnglRussLish(){
         eng.add(getSet);
     }return eng;
 }
-//
-//static ArrayList<String>rusian(){
-//    ArrayList<String>ru=new ArrayList<>();
-//        for (String s:WraitFile.lessonFile2()){
-//            String[] s2=s.split("\\-",2);
-//ru.add(s2[1]);
-//    }return ru;
-//}
 
 
-//    public static void zadanie1(){
-//    for (String s : WraitFile.lessonFile2()){
-//    System.out.println(s);
-//    sc.nextLine();}
-//    for (int i=0;i<rusian().size();i++){
-//        System.out.println(english().get(i));
-//        System.out.println("правельный ответ:");
-//        int x=0;
-//        for(String s:rusian()){
-//            x++;
-//            System.out.println(x+":"+s);
-//
-//        }String ss =sc.nextLine();
-//        String z=String.valueOf(i+1);
-//        while (!ss.equals(z)) {
-//            System.out.println("ответ не верный");
-//            System.out.println("попробуйте еще раз");
-//            ss=sc.nextLine();
-//        }
-//        System.out.println("вы ответили верно ");
-//    }
-//}
+    public static void zadanie1(){
+    for (String s : WraitFile.lessonFile2()){
+    System.out.println(s);
+    sc.nextLine();}
+    for (int i=0;i<learEnglRussLish().size();i++){
+        System.out.println(learEnglRussLish().get(i).getEnglish());
+        System.out.println("правельный ответ:");
+        int x=0;
+        for(GetSet s:learEnglRussLish()){
+            x++;
+            System.out.println(x+":"+s.getEnglish());
+
+        }String ss =sc.nextLine();
+        String z=String.valueOf(i+1);
+        while (!ss.equals(z)) {
+            System.out.println("ответ не верный");
+            System.out.println("попробуйте еще раз");
+            ss=sc.nextLine();
+        }
+        System.out.println("вы ответили верно ");
+    }
+}
 }
