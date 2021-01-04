e84a0de464d369a3d0dbffe18d2d8de15d596d4b
VladimirKing <cheremuha1@gmail.com>
2021-01-04 21:29:29 +0300

    xxx

--- src/Learning.java
+++ src/Learning.java
@@ -14,7 +14,24 @@ public class Learning {
 //    for (String s : WraitFile.lessonFile2()){
 //    System.out.println(s);
 //    sc.nextLine();}
+WraitFile.learEnglRussLish().stream().forEach((GetSet ii)->{
+    int y=0;y++;
+    System.out.println(ii.getRusian());
+    System.out.println("правельный ответ");
 
+    WraitFile.learEnglRussLish().stream().forEach((GetSet iu) ->  {
+        int x=0;
+        System.out.println(x+" "+iu.getEnglish());
+        x++;
+    });
+    String ss= sc.nextLine();
+    while (!ss.equals(String.valueOf(y))){
+        System.out.println("ответ не верный ");
+        System.out.println("попробуйте еще раз:");
+        ss=sc.nextLine();
+    }
+    System.out.println("ты молодец");
+});
     for (int i = 0; i< WraitFile.learEnglRussLish().size(); i++){
         System.out.println(WraitFile.learEnglRussLish().get(i).getEnglish());
         System.out.println("правельный ответ:");
@@ -22,8 +39,8 @@ public class Learning {
         for(GetSet s: WraitFile.learEnglRussLish()){
             x++;
             System.out.println(x+":"+s.getEnglish());
-
-        }String ss =sc.nextLine();
+        }
+        String ss =sc.nextLine();
         String z=String.valueOf(i+1);
         while (!ss.equals(z)) {
             System.out.println("ответ не верный");
