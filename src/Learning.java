e3de81cc5465e8aecb1c5272ac33e4f0c6849b96
VladimirKing <cheremuha1@gmail.com>
2021-01-05 19:40:39 +0300

    puhs

--- src/Learning.java
+++ src/Learning.java
@@ -14,24 +14,25 @@ public class Learning {
 //    for (String s : WraitFile.lessonFile2()){
 //    System.out.println(s);
 //    sc.nextLine();}
-WraitFile.learEnglRussLish().stream().forEach((GetSet ii)->{
-    int y=0;y++;
-    System.out.println(ii.getRusian());
-    System.out.println("правельный ответ");
 
-    WraitFile.learEnglRussLish().stream().forEach((GetSet iu) ->  {
-        int x=0;
-        System.out.println(x+" "+iu.getEnglish());
-        x++;
-    });
-    String ss= sc.nextLine();
-    while (!ss.equals(String.valueOf(y))){
-        System.out.println("ответ не верный ");
-        System.out.println("попробуйте еще раз:");
-        ss=sc.nextLine();
-    }
-    System.out.println("ты молодец");
-});
+        // x - как счетчик не считает, незнаю как правильно зделать, в лямде это по другому работает
+//WraitFile.learEnglRussLish().stream().forEach((GetSet ii)->{
+//    int y=0;y++;
+//    System.out.println(ii.getRusian());
+//    System.out.println("правельный ответ");
+//    WraitFile.learEnglRussLish().stream().forEach((GetSet iu) ->  {
+//        int x=0;
+//        System.out.println(x+" "+iu.getEnglish());
+//        x++;
+//    });
+//    String ss= sc.nextLine();
+//    while (!ss.equals(String.valueOf(y))){
+//        System.out.println("ответ не верный ");
+//        System.out.println("попробуйте еще раз:");
+//        ss=sc.nextLine();
+//    }
+//    System.out.println("ты молодец");
+//});
     for (int i = 0; i< WraitFile.learEnglRussLish().size(); i++){
         System.out.println(WraitFile.learEnglRussLish().get(i).getEnglish());
         System.out.println("правельный ответ:");
