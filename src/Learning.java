f57cb55a3af83ce0d1400cf991127dbbf0092086
VladimirKing <cheremuha1@gmail.com>
2021-01-09 04:32:57 +0300

    aa

--- src/Learning.java
+++ src/Learning.java
@@ -5,9 +5,8 @@ public class Learning {
     static Scanner sc = new Scanner(System.in);
 
 
-
-    public static void zadanie1(){
-        WraitFile.lessonFile2().stream().forEach((String s)->{
+    public static void zadanie1() {
+        WraitFile.lessonFile2().stream().forEach((String s) -> {
             System.out.println(s);
             sc.nextLine();
         });
@@ -21,23 +20,24 @@ public class Learning {
 //    int y=0;y++;
 //    System.out.println(ii.getRusian());
 //    System.out.println("правельный ответ");
+        // как понять эту лямду
 
-    for (int i = 0; i< WraitFile.learEnglRussLish().size(); i++){
-        System.out.println(WraitFile.learEnglRussLish().get(i).getEnglish());
-        System.out.println("правельный ответ:");
-        int x=0;
-        for(GetSet s: WraitFile.learEnglRussLish()){
-            x++;
-            System.out.println(x+":"+s.getEnglish());
+        for (int i = 0; i < WraitFile.learEnglRussLish().size(); i++) {
+            System.out.println(WraitFile.learEnglRussLish().get(i).getEnglish());
+            System.out.println("правельный ответ:");
+            int x = 0;
+            for (GetSet s : WraitFile.learEnglRussLish()) {
+                x++;
+                System.out.println(x + ":" + s.getEnglish());
+            }
+            String ss = sc.nextLine();
+            String z = String.valueOf(i + 1);
+            while (!ss.equals(z)) {
+                System.out.println("ответ не верный");
+                System.out.println("попробуйте еще раз");
+                ss = sc.nextLine();
+            }
+            System.out.println("вы ответили верно ");
         }
-        String ss =sc.nextLine();
-        String z=String.valueOf(i+1);
-        while (!ss.equals(z)) {
-            System.out.println("ответ не верный");
-            System.out.println("попробуйте еще раз");
-            ss=sc.nextLine();
-        }
-        System.out.println("вы ответили верно ");
     }
-}
 }
