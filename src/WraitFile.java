cb1000cefa1c35bacc56835fc92c3874a2931dca
VladimirKing <cheremuha1@gmail.com>
2021-02-08 17:28:40 +0200

    nn

--- src/WraitFile.java
+++ src/WraitFile.java
@@ -5,30 +5,33 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class WraitFile {
-static ArrayList<String> lessonFile2(){
-    ArrayList<String> lessonFile=new ArrayList();
-    try {
-           Scanner filt=new Scanner(new File("lesson.txt"));
+    static ArrayList<String> lessonFile2() {
+        ArrayList<String> lessonFile = new ArrayList();
+        try {
+            Scanner filt = new Scanner(new File("lesson.txt"));
 
-           while (filt.hasNextLine()){
+            while (filt.hasNextLine()) {
 
-               String  fi= filt.nextLine();
-               lessonFile.add(fi);
-           }filt.close();
-       } catch (FileNotFoundException e) {
-           e.printStackTrace();
-       }
-    return lessonFile;
-}
-    static ArrayList<GetSet>learEnglRussLish(){
-        ArrayList<GetSet> eng=new ArrayList<>();
-        for (String s:lessonFile2()){
-            String[] s2=s.split("\\-",2);
+                String fi = filt.nextLine();
+                lessonFile.add(fi);
+            }
+            filt.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return lessonFile;
+    }
+
+    static ArrayList<GetSet> learEnglRussLish() {
+        ArrayList<GetSet> eng = new ArrayList<>();
+        for (String s : lessonFile2()) {
+            String[] s2 = s.split("-" , 2);
             GetSet getSet = new GetSet();
             getSet.setEnglish(s2[0]);
             getSet.setRusian(s2[1]);
             eng.add(getSet);
-        }return eng;
+        }
+        return eng;
     }
 
-    }
+}
