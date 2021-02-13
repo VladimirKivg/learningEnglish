20a5852cf46a2c54055e4048532e9caff1ba0209
VladimirKing <cheremuha1@gmail.com>
2021-02-13 22:53:35 +0200

    m

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 public class Main {
 //static String n=WraitFile.wraitFile();
 
-
     public static void main(String[] args) {
         WraitFile n =new WraitFile();
       //  Learning.learning();
